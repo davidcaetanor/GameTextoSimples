e34879921c3cb740b0ec1bfaa68d2da13800b9ff
daka1 <davidcaetanoribeiro@gmail.com>
2025-04-04 11:45:17 -0300

    Primeiras alterações no arquivo JOGO (main)
    Alteração principal: renomeação da classe principal para 'Jogo', conforme exigido no exercício. Além disso, foi utilizado o recurso de JOptionPane em vez de System.out ou Scanner, conforme as solicitacoes do exercicio

---
+++ src/Jogo.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Jogo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Bem-vindo ao Desafio do Sobrevivente!");
+
+        JOptionPane.showMessageDialog(null,"Seu objetivo é fazer com que seu personagem alcance 10 pontos de experiência.");
+        JOptionPane.showMessageDialog(null,"Cuidado! O jogo termina se sua energia chegar a 0 ou se os níveis de fome e sono atingirem o limite.");
+        JOptionPane.showMessageDialog(null,"Cada vez que você caçar, ganhará 1 ponto de experiência.");
+
+        JOptionPane.showMessageDialog(null,"Gerencie bem seus recursos e boa sorte!");
+
+    }
+}

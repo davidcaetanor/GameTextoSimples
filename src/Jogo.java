82d8b01b44ed58d72dddebf220cb000be938c080
daka1 <davidcaetanoribeiro@gmail.com>
2025-04-04 11:53:07 -0300

    Layout pré criacao da instancia
    Layout do codigo executavel  JOGO antes de unificar com a branch Personagem

--- src/Jogo.java
+++ src/Jogo.java
@@ -5,10 +5,15 @@ public class Jogo {
         JOptionPane.showMessageDialog(null,"Bem-vindo ao Desafio do Sobrevivente!");
 
         JOptionPane.showMessageDialog(null,"Seu objetivo é fazer com que seu personagem alcance 10 pontos de experiência.");
-        JOptionPane.showMessageDialog(null,"Cuidado! O jogo termina se sua energia chegar a 0 ou se os níveis de fome e sono atingirem o limite.");
+        JOptionPane.showMessageDialog(null,"Cuidado! O jogo termina se sua energia chegar a 0 ou se os níveis de fome e sono atingirem o limite (10).");
         JOptionPane.showMessageDialog(null,"Cada vez que você caçar, ganhará 1 ponto de experiência.");
 
         JOptionPane.showMessageDialog(null,"Gerencie bem seus recursos e boa sorte!");
 
+        String nome = JOptionPane.showInputDialog(null,"Insira o nome do Personagem: ");
+        int energia = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a Energia do Personagem (Recomendado 10): "));
+        int fome = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a Fome do Personagem (Recomendado 0): "));
+        int sono = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe o Sono do Personagem (Recomendado 0): "));
+        
     }
 }
